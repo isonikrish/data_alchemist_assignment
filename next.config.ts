0804db0a0435b8546bdf8c9e342501026830e57d
Krish Soni <isonikrish@gmail.com>
2025-06-29 16:37:40 +0530

    ts shouldnt exist

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from "next";
+import path from "path";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
